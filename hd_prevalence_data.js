f8b35eaaf5e1ccc210564fcb35c49a4fbc2b3cf8
ocords <ocords@stanford.edu>
2015-09-05 22:41:59 -0700

    Initial commit

---
+++ hd_prevalence_data.js
@@ -0,0 +1,342 @@
+hd_data =
+  [
+    {
+      "Country Code":"AU",
+      "Location":"Australia (Queensland)",
+      "Date":"1976",
+      "Prevalence/100,000":5.8,
+      "Reference":"(Wallace & Parker 1979)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/australia/"
+    },
+    {
+      "Country Code":"AT",
+      "Location":"Austria",
+      "Date":"1993-1997",
+      "Prevalence/100,000":10,
+      "Reference":"(Laccone et al 1999)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"AZ",
+      "Location":"Azerbaijan",
+      "Date":"1985",
+      "Prevalence/100,000":23.8,
+      "Reference":"(Al-Jader et al 2001)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/asia/"
+    },
+    {
+      "Country Code":"BE",
+      "Location":"Belgium (Liege, Limbourg, Namur, Luxembourg)",
+      "Date":"1970",
+      "Prevalence/100,000":1.6,
+      "Reference":"(Husquinet 1973)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"CA",
+      "Location":"Canada (Manitoba & Saskatchewan)",
+      "Date":"1975",
+      "Prevalence/100,000":8.4,
+      "Reference":"(Shokeir 1975)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/north-america/"
+    },
+    {
+      "Country Code":"CN",
+      "Location":"China (Hong Kong)",
+      "Date":"1984-1991",
+      "Prevalence/100,000":0.4,
+      "Reference":"(Leung et al 1992)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/asia/"
+    },
+    {
+      "Country Code":"HR",
+      "Location":"Croatia",
+      "Date":"2002",
+      "Prevalence/100,000":1,
+      "Reference":"(Hecimovic et al 2002)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"EG",
+      "Location":"Egypt (Assiut)",
+      "Date":"1988-1990",
+      "Prevalence/100,000":21,
+      "Reference":"(Kandil et al 1994)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/africa/"
+    },
+    {
+      "Country Code":"FI",
+      "Location":"Finland",
+      "Date":"1986",
+      "Prevalence/100,000":0.5,
+      "Reference":"(Palo et al 1987)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"FR",
+      "Location":"France (North-West)",
+      "Date":"",
+      "Prevalence/100,000":5,
+      "Reference":"(Petit & Salomez 1985)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"DE",
+      "Location":"Germany",
+      "Date":"1993-1997",
+      "Prevalence/100,000":10,
+      "Reference":"(Laccone et al 1999)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"IS",
+      "Location":"Iceland",
+      "Date":"1963",
+      "Prevalence/100,000":2.7,
+      "Reference":"(Gudmundsson 1969)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"IN",
+      "Location":"India (Pakistan, Punjab and Gujerat)",
+      "Date":"1990",
+      "Prevalence/100,000":1.7,
+      "Reference":"(Shiwach & Lindenbaum 1990)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/asia/"
+    },
+    {
+      "Country Code":"IT",
+      "Location":"Italy (Florence)",
+      "Date":"1970-1979",
+      "Prevalence/100,000":4.1,
+      "Reference":"(Groppi et al 1986)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"JP",
+      "Location":"Japan (Aichi)",
+      "Date":"1959",
+      "Prevalence/100,000":0.4,
+      "Reference":"(Kishimoto et al 1957)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/asia/"
+    },
+    {
+      "Country Code":"MR",
+      "Location":"Mauritius (European descent)",
+      "Date":"1977",
+      "Prevalence/100,000":46.2,
+      "Reference":"(Hayden et al 1981)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"MX",
+      "Location":"Mexico",
+      "Date":"2008",
+      "Prevalence/100,000":4,
+      "Reference":"(Alonso et al 2009)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/north-america/"
+    },
+    {
+      "Country Code":"NZ",
+      "Location":"New Zealand",
+      "Date":"",
+      "Prevalence/100,000":5.7,
+      "Reference":"(Harper 1996)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/australia/"
+    },
+    {
+      "Country Code":"NG",
+      "Location":"Nigeria (Ibadan)",
+      "Date":"",
+      "Prevalence/100,000":0.2,
+      "Reference":"(Aiyesimoju et al 1984)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/africa/"
+    },
+    {
+      "Country Code":"NO",
+      "Location":"Norway",
+      "Date":"1950",
+      "Prevalence/100,000":5.8,
+      "Reference":"(Saugstad & Odegård 1986)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"PL",
+      "Location":"Poland (Pruszkow)",
+      "Date":"1960",
+      "Prevalence/100,000":4.8,
+      "Reference":"(Al-Jader et al 2001)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"RU",
+      "Location":"Russia (Vladimir Oblast)",
+      "Date":"",
+      "Prevalence/100,000":1.9,
+      "Reference":"(Kirilenko et al 2004)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"SI",
+      "Location":"Slovenia",
+      "Date":"2006",
+      "Prevalence/100,000":5.2,
+      "Reference":"(Peterlin et al 2008)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"ZA",
+      "Location":"South Africa (White and Coloured population)",
+      "Date":"1979",
+      "Prevalence/100,000":2.2,
+      "Reference":"(Hayden et al 1980)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/africa/"
+    },
+    {
+      "Country Code":"ES",
+      "Location":"Spain (Valencia)",
+      "Date":"1987-1992",
+      "Prevalence/100,000":5.4,
+      "Reference":"(Burguera et al 1997)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"SE",
+      "Location":"Sweden",
+      "Date":"1985",
+      "Prevalence/100,000":5.6,
+      "Reference":"(Al-Jader et al 2001)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"CH",
+      "Location":"Switzerland",
+      "Date":"1993-1997",
+      "Prevalence/100,000":10,
+      "Reference":"(Laccone et al 1999)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"TZ",
+      "Location":"Tanzania (Mount Kilimanjaro, Bantu community)",
+      "Date":"1980",
+      "Prevalence/100,000":7,
+      "Reference":"(Scrimgeour 1981)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/africa/"
+    },
+    {
+      "Country Code":"GB",
+      "Location":"UK (England, Oxford region)",
+      "Date":"1985",
+      "Prevalence/100,000":5.7,
+      "Reference":"(Shiwach 1994)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"IE",
+      "Location":"UK (Ireland, Northern)",
+      "Date":"1991",
+      "Prevalence/100,000":6.4,
+      "Reference":"(Morrison et al 1995)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/europe/"
+    },
+    {
+      "Country Code":"US",
+      "Location":"USA (Maryland)",
+      "Date":"1980",
+      "Prevalence/100,000":5.2,
+      "Reference":"(Folstein et al 1987)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/north-america/"
+    },
+    {
+      "Country Code":"VE",
+      "Location":"Venezuela (Lake Maracaibo)",
+      "Date":"1973",
+      "Prevalence/100,000":699.3,
+      "Reference":"(Avila-Giron 1973)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/global-hd/south-america/"
+    },
+    {
+      "Country Code":"ZW",
+      "Location":"Zimbabwe (Manicaland region, Shona population)",
+      "Date":"1988-1989",
+      "Prevalence/100,000":0.8,
+      "Reference":"(Scrimgeour & Pfumojena 1992)",
+      "Website":"http://web.stanford.edu/group/hopes/cgi-bin/hopes_test/category/africa/"
+    }
+  ];
+
+
+var prevalence_data = hd_data.reduce(function(map, obj) { map[obj["Country Code"]] = obj["Prevalence/100,000"];
+  return map;
+}, {});
+var location_data = hd_data.reduce(function(map, obj) {
+  map[obj["Country Code"]] = obj["Location"];
+  return map;
+}, {});
+var links = hd_data.reduce(function(map, obj) {
+  map[obj["Country Code"]] = obj["Website"];
+  return map;
+}, {});
+
+var ONE = ".1 - .5";
+var TWO = ".5 - 1";
+var THREE = "1 - 5";
+var FOUR = "> 5";
+var FIVE = "unknown";
+
+var color_map = {};
+color_map[ONE]   = "#EECDD6";
+color_map[TWO] = "#D48298";
+color_map[THREE]   = "#C25070";
+color_map[FOUR]   = "#A80532";
+color_map[FIVE]   = "#FFFFFF";
+
+var colors = {};
+Object.keys(prevalence_data).forEach(function(key) {
+  var value = prevalence_data[key];
+  if (value < .1) {
+    colors[key] = FIVE;
+  } else if (value < .5) {
+    colors[key] = ONE;
+  } else if (value < 1.) {
+    colors[key] = TWO;
+  } else if (value < 5) {
+    colors[key] = THREE;
+  } else {
+    colors[key] = FOUR;
+  }
+});
+
+$('#map').vectorMap({
+  map: 'world_mill',
+  series: {
+    regions: [{
+      data: prevalence_data,
+      values: colors,
+      scale: color_map,
+      legend: {
+        vertical: true,
+        title: 'Prevalence/100,000'
+      }
+    }]
+  },
+  onRegionClick: function(e, code) {
+    if (code in links) {
+      window.location = links[code];
+    }
+  },
+  onRegionTipShow: function(e, el, code) {
+   if (code in location_data) {
+     el.html('<href a='+links[code]+'>'+location_data[code]+': '+prevalence_data[code]+'</href>');
+   } else {
+     e.preventDefault();
+   }
+  }
+});
+
+$('#map').resize();
+
+window.onload = function() {
+ $('#map').resize();
+};
